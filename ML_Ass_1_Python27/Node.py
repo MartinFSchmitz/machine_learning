6e8807e94b790ab201a41657b82440c15b4dd336
MartinFSchmitz <martins@uni-koblenz.de>
2017-11-08 16:55:26 +0100

    added python27 version and use_tree class

---
+++ ML_Ass_1_Python27/Node.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  5 12:24:13 2017
+
+@author: marti
+"""
+
+class Node(object):
+    def __init__(self, used_atts = [], examples = None, leaf = False, label = None):
+        
+        self.used_atts = used_atts
+        self.examples = examples
+        
+        self.left = None
+        self.right = None
+        
+        self.split_attr = None
+        self.split_value = None
+        self.label = label
+        self.leaf = leaf
+
+        
+    def becomes_leaf(node,c):
+        node.leaf = True
+        node.label = c
+        
+
---
+++ ML_Ass_1_Python27/Pydot_test.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov  5 12:24:13 2017
+
+@author: marti
+"""
+import numpy as np
+import pandas as pd
+import copy
+import pydot
+import os
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+
+
+
+# first you create a new graph, you do that with pydot.Dot()
+graph = pydot.Dot(graph_type='digraph')
+label= "Hallo = %d %d" % (5,4)
+node_a = pydot.Node("Node A", shape = "box", label= label , style="solid", fillcolor="red")
+graph.add_node(node_a)
+
+
+node_b = pydot.Node("Node B", shape = "box", style="solid", fillcolor="red")
+graph.add_node(node_a)
+node_c = pydot.Node("Node C", shape = "box", style="solid", fillcolor="red")
+graph.add_node(node_a)
+
+#graph.add_edge(pydot.Edge(node_a, node_b))
+
+#graph.add_edge(pydot.Edge(node_a, node_c))
+#graph.add_edge(pydot.Edge(node_a, node_c, label="and back we go again", labelfontcolor="#009933", fontsize="10.0", color="blue"))
+
+graph.write_png('example1_graph.png')
+
+# and we are done!
