c35d3f9e98fe6e237184eabfa4dbfe233a68f716
MartinFSchmitz <martins@uni-koblenz.de>
2017-12-05 15:16:03 +0100

    prettier kNN

--- ML_KNN/kNN.py
+++ ML_KNN/kNN.py
@@ -8,10 +8,13 @@ import pandas as pd
 import copy
 import pickle
 from scipy.spatial.distance import cosine
+import sys
+import time
 
-import warnings
-#warnings.filterwarnings("ignore")
+"""            Hyper-Parameters            """
+K = 3
 
+#------------------------------------------------------
 
 def normalize(train, test):
     """ normalize data sets: search for highest entry for attributes
@@ -44,7 +47,7 @@ def modify_data(training, test):
     test = compute_new_values(test, means, std_der)    
     return training, test
         
-def get_neighbors(training, sample):
+def get_neighbors(training, sample, k):
     
     dist_row = pd.DataFrame({"distance": np.zeros(len(training))}) # create empty dist row
     train_with_dist = copy.copy(pd.concat([training, dist_row], axis = 1)) # append distrow to training data
@@ -55,31 +58,30 @@ def get_neighbors(training, sample):
         
     sorted_tuples = train_with_dist.sort_values(by="distance", ascending = True)  # sort list after attribute attr
     # now the k first entrys in sorted tuples are the k nearest neighbors of test-sample
-    #print(cosine(sample, sorted_tuples[0])
-    return sorted_tuples[:K] # return k first elements as k nearest neighbors
+    return sorted_tuples[:k] # return k first elements as k nearest neighbors
 
     
-def get_prediction(training, sample):
-    neighbors = get_neighbors(training, sample)
-    #print(neighbors)
+def get_prediction(training, sample, k):
+    neighbors = get_neighbors(training, sample, k)
     if neighbors.class_label.mean() >= 0.5: # easy implementation of a majority vote
         return 1
     else:
         return 0
 
 
-def predict_all(training,test):
+def compute_accuracy(training,test, k):
     correct_sampled = 0
-    for sample in range (len(test)):   
-        sample_class = get_prediction(training, test.iloc[sample])
-        #print(sample_class)
-        #print(test.iloc[sample].class_label)
+    for sample in range (len(test)):  
+        #print(str(round((sample/float(len(test)))*100,2)) + "%  done")
+        sample_class = get_prediction(training, test.iloc[sample], k)
         if sample_class == test.iloc[sample].class_label:
+            
             correct_sampled +=1
-
+    print("K = " + k)
     print("Accuracy: " + str(correct_sampled / float(len(test))))
         
 def get_csv_plus_header(data):
+    """ read data and add header line """
     header = []
     csv_len = len(pd.read_csv(data).columns)
     for i in range (csv_len-1):
@@ -88,17 +90,17 @@ def get_csv_plus_header(data):
     header.append("class_label")
     csv = pd.read_csv(data, names = header)
     return csv
-    
-#------------------------------------------------------
-"""            Hyper-Parameters            """
-K = 3
+
+
 #------------------------------------------------------
 
 # Main
 training = get_csv_plus_header('spam_training.csv')
 test = get_csv_plus_header('spam_test.csv')
-
 training, test = modify_data(training, test)
-predict_all(training, test)
-
-
+print("start computation with K = 1")
+compute_accuracy(training, test, 1)
+print("start computation with K = 3")
+compute_accuracy(training, test, 3)
+print("start computation with K = 5")
+compute_accuracy(training, test, 5)
